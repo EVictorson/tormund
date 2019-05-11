cefa92fa9fed97a3bd7fd53aa03174d741b4aa59
Eric Victorson <vier0801@gmail.com>
2019-05-11 09:05:55 -0500

    add submodule and change udev install

--- comms/udev_rules/install_udev_rules.sh
+++ comms/udev_rules/install_udev_rules.sh
@@ -1,8 +1,12 @@
 #!/bin/bash
 
+echo "Deleting previous udev rules if they exist..."
+sudo rm /etc/udev/rules.d/99-arduino-uno-serial.rules
+sudo rm /etc/udev/rules.d/99-rplidara2.rules
+
 echo "Copying udev rules..."
 sudo cp 99-arduino-uno-serial.rules /etc/udev/rules.d
 sudo cp 99-rplidara2.rules /etc/udev/rules.d
 
-sudo udevadm control --reload
+sudo udevadm control --reload-rules && sudo service udev restart && sudo udevadm trigger
 echo "Done!"
