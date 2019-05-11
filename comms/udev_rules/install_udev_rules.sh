078c6ff106362772ffa3e5a7e0e1374153dee9f3
Phil Weber <phillip.weber@honeywell.com>
2019-05-10 23:05:24 -0500

    change rplidar udev rule to set mode to 777

--- comms/udev_rules/install_udev_rules.sh
+++ comms/udev_rules/install_udev_rules.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 
-cd /etc/udev/rules.d
-sudo ln -s ~/vcs/tormund/comms/udev_rules/99-arduino-uno-serial.rules 99-arduino-uno-serial.rules
-sudo ln -s ~/vcs/tormund/comms/udev_rules/99-rplidara2.rules 99-rplidara2.rules
+echo "Copying udev rules..."
+sudo cp 99-arduino-uno-serial.rules /etc/udev/rules.d
+sudo cp 99-rplidara2.rules /etc/udev/rules.d
 
 sudo udevadm control --reload
+echo "Done!"
